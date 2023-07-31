11cf268465d9925f87e266c822bfe372679d7291
hafizakbar19 <hafizakbar19@gmail.com>
2023-07-31 16:10:12 +0500

    simple React app

--- code/2-component-data/start/src/RecipeTitle.jsx
+++ code/2-component-data/start/src/RecipeTitle.jsx
@@ -1,3 +1,15 @@
 import React from 'react';
+import './RecipeTitle.css';
 
 // TODO: Create RecipeTitle component
+ function RecipeTitles(props) {
+    return(
+        <section>
+            <h2>{props.title}</h2>
+            <h3 className={ props.feedback.rating <= 3.5 ? 'red' : 'green' }>
+            { props.feedback.rating } from { props.feedback.reviews } reviews
+</h3>
+        </section>
+    )    
+}
+export default RecipeTitles; 
